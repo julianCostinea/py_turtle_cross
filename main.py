b02c035f92a062259b3a21a43e8758e43c0de8a9
Julian <julian.costinea@gmail.com>
2024-04-30 19:04:26 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle Crossing")
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.05)
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect player reaching top
+    if player.ycor() > 280:
+        scoreboard.increase_score()
+        player.go_to_start()
+        car_manager.level_up()
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-100, 260)
+        self.write(self.score, align="center", font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER. Your score was: {self.score}", align="center", font=FONT)
